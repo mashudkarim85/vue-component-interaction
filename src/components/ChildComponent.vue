cf86bc30995976e95726bb1b7eedc634cd62037a
mashud.karim <mashud.karim85@gmail.com>
2023-08-19 18:20:08 +0600

    completed task

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+  message: String,
+});
+const emit  = defineEmits();
+  
+const emitMessage = () => {
+    emit('messageEmitted', 'Custom message from Child Component');
+};
+</script>
+
+
+<template>
+    <div>
+      <p>Message from ParentComponent: {{ props.message }}</p>
+      <button class="bg-red-500 my-5 text-white" @click="emitMessage">Emit Message</button>
+    </div>
+  </template>
+  
+
+  
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue'
+import ChildComponent from './ChildComponent.vue'
+
+const messageFromParent = 'Hello my child from Parent Component';
+let emittedMessage = ref('');
+
+const handleMessageEmitted = (message) => {
+  emittedMessage.value = message;
+};
+
+</script>
+
+<template>
+    <div>
+        <ChildComponent :message="messageFromParent" @messageEmitted="handleMessageEmitted" />
+        <p>Message from ChildComponent: {{ emittedMessage }}</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
